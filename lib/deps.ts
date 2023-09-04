4d293f1cff889c31680cad6a33c278b0f3a1db6e
nanikit <nanikit4@gmail.com>
2023-09-04 21:20:00 +0900

    refactor: deduplicate deps

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,11 +1,8 @@
-export {
-  fromFileUrl,
-  resolve,
-} from "https://deno.land/std@0.201.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.201.0/flags/mod.ts";
+export type * from "npm:ky";
+export { default as ky } from "npm:ky";
 export * from "npm:rollup";
+export { fromFileUrl, resolve } from "../deps.ts";
 
 // @deno-types="npm:@types/lodash-es"
 export * from "npm:lodash-es";
-
-export type * from "npm:ky";
-export { default as ky } from "npm:ky";
--- test/deps.ts
+++ test/deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/std@0.201.0/assert/mod.ts";
-export * from "https://deno.land/std@0.201.0/path/mod.ts";
 export * from "https://deno.land/std@0.201.0/testing/mock.ts";
+export * from "../deps.ts";
