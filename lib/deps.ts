5e67068b2ee88b091ab260676d1b79760385b956
nanikit <nanikit4@gmail.com>
2023-09-05 05:14:07 +0900

    feat: make entrypoint

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,7 +1,9 @@
 export { parse } from "https://deno.land/std@0.201.0/flags/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.201.0/fs/expand_glob.ts";
 export type * from "npm:ky";
 export { default as ky } from "npm:ky";
 export * from "npm:rollup";
+// export { default as rollup } from "npm:rollup";
 export { fromFileUrl, resolve } from "../deps.ts";
 
 // @deno-types="npm:@types/lodash-es"
