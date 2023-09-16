c1784b95749c26dfe5f2848005c05ef3637b8388
nanikit <nanikit4@gmail.com>
2023-09-16 22:47:43 +0900

    test: make atomic for editing input data

--- test/header_helpers_test.ts
+++ test/header_helpers_test.ts
@@ -1,67 +1,97 @@
 import { extractUserscriptHeader, mergeHeader } from "../lib/header_helpers.ts";
 import { assertEquals } from "./deps.ts";
-import { scriptPaths } from "./test_helpers.ts";
 
-Deno.test("Given user script", async (test) => {
-  const script = await Deno.readTextFile(scriptPaths.example);
+Deno.test("Given userscript header parser", async (test) => {
+  const parse = extractUserscriptHeader;
 
-  await test.step("when parse the header", async (test) => {
-    const parsed = extractUserscriptHeader(script);
+  await test.step("when parse no userscript", async (test) => {
+    const parsed = parse(`console.log('hello world')`);
 
-    await test.step("it should be correct", () => {
-      assertEquals(parsed, {
-        "@name": ["main userscript"],
-        "@name:ko": ["메인 스크립트"],
-        "@description": ["for test"],
-        "@description:ko": ["테스트"],
-        "@version": ["{date_version}"],
-        "@namespace": ["https://greasyfork.org/en/users/713014-nanikit"],
-        "@exclude": ["*"],
-        "@match": ["http://unused-field.space/"],
-        "@author": ["nanikit"],
-        "@grant": ["GM_getValue"],
-        "@require": ["https://cdn.jsdelivr.net/npm/requirejs@2.3.6/require.js"],
+    await test.step("it should be undefined", () => {
+      assertEquals(parsed, undefined);
+    });
+  });
+
+  await test.step("when parse header including name", async (test) => {
+    const parsed = parse(`// ==UserScript==
+// @name main userscript
+// ==/UserScript==`);
+
+    await test.step("it should include name", () => {
+      assertEquals(parsed, { "@name": ["main userscript"] });
+    });
+  });
+
+  await test.step("when parse header including grants", async (test) => {
+    const parsed = parse(`// ==UserScript==
+// @grant GM_getValue
+// @grant GM_setValue
+// ==/UserScript==`);
+
+    await test.step("it should include grants", () => {
+      assertEquals(parsed, { "@grant": ["GM_getValue", "GM_setValue"] });
+    });
+  });
+});
+
+Deno.test("Given userscript header merger", async (test) => {
+  const merge = mergeHeader;
+
+  await test.step("when merge two empties", async (test) => {
+    const merged = merge({}, {});
+
+    await test.step("it should return empty", () => {
+      assertEquals(merged, {});
+    });
+  });
+
+  await test.step("when merge a header having name and an empty", async (test) => {
+    const merged = merge({ "@name": ["main userscript"] }, {});
+
+    await test.step("it should return name", () => {
+      assertEquals(merged, { "@name": ["main userscript"] });
+    });
+  });
+
+  await test.step("when merge two headers having name", async (test) => {
+    const merged = merge({ "@name": ["main userscript"] }, {
+      name: ["sub userscript"],
+    });
+
+    await test.step("it should return first name", () => {
+      assertEquals(merged, { "@name": ["main userscript"] });
+    });
+  });
+
+  await test.step("when merge two headers having resource", async (test) => {
+    const merged = merge({
+      "@resource": [
+        "react https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js",
+      ],
+    }, {
+      "@resource": [
+        "react-dom https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js",
+      ],
+    });
+
+    await test.step("it should align column and return all resources", () => {
+      assertEquals(merged, {
         "@resource": [
-          "@stitches/react  https://cdn.jsdelivr.net/npm/@stitches/react@1.2.8/dist/index.cjs",
-          "react-dom        https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js",
-          "react            https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js",
-          "library1         file://library1.user.js",
+          "react     https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js",
+          "react-dom https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js",
         ],
       });
     });
   });
 
-  await test.step("when append the header", async (test) => {
-    const script1 = await Deno.readTextFile(scriptPaths.library1);
+  await test.step("when merge two headers having same grants", async (test) => {
+    const merged = merge(
+      { "@grants": ["GM_getValue"] },
+      { "@grants": ["GM_getValue"] },
+    );
 
-    const first = extractUserscriptHeader(script);
-    const second = extractUserscriptHeader(script1);
-    const merged = mergeHeader(first!, second!);
-
-    await test.step("it should be correct", () => {
-      assertEquals(merged, {
-        "@name": ["main userscript"],
-        "@name:ko": ["메인 스크립트"],
-        "@description": ["for test"],
-        "@description:ko": ["테스트"],
-        "@version": ["{date_version}"],
-        "@namespace": ["https://greasyfork.org/en/users/713014-nanikit"],
-        "@exclude": ["*"],
-        "@match": ["http://unused-field.space/"],
-        "@author": ["nanikit"],
-        "@grant": [
-          "GM_getValue",
-          "GM_setValue",
-        ],
-        "@require": ["https://cdn.jsdelivr.net/npm/requirejs@2.3.6/require.js"],
-        "@resource": [
-          "@stitches/react https://cdn.jsdelivr.net/npm/@stitches/react@1.2.8/dist/index.cjs",
-          "library1        file://library1.user.js",
-          "library2        http://localhost:8080/library2.user.js",
-          "react           https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js",
-          "react-dom       https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js",
-        ],
-      });
+    await test.step("it should return one grants", () => {
+      assertEquals(merged, { "@grants": ["GM_getValue"] });
     });
   });
 });
